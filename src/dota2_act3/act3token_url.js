453946813b2650bf7d692f3fa9fb506c85951ab0
Cheung Zirpon <735037139@qq.com>
2024-07-21 01:58:17 +0800

    增加页面按钮 就不用点那么多了

--- src/dota2_act3/act3token_url.js
+++ src/dota2_act3/act3token_url.js
@@ -1,24 +1,84 @@
 const act3token_url = {
-  flame: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/flame.png',
-  frost: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/frost.png',
-  storm: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/storm.png',
-  tide: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/tide.png',
-  nature: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/nature.png',
-  void: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/void.png',
-  blood: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/blood.png',
-  mystic: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/mystic.png',
-  armor: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/armor.png',
-  helmet: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/helmet.png',
-  shield: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/shield.png',
-  robe: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/robe.png',
-  cape: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/cape.png',
-  mask: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/mask.png',
-  fur: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/fur.png',
-  scale: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/scale.png',
-  minion: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/minion.png',
-  illusion: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/illusion.png',
-  ward: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/ward.png',
-  partner: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/partner.png',
+  flame: {
+    name: '火焰',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/flame.png',
+  },
+  frost: {
+    name: '冰霜',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/frost.png',
+  },
+  storm: {
+    name: '风暴',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/storm.png',
+  },
+  tide: {
+    name: '潮汐',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/tide.png',
+  },
+  nature: {
+    name: '自然',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/nature.png',
+  },
+  void: {
+    name: '虚空',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/void.png',
+  },
+  blood: {
+    name: '鲜血',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/blood.png',
+  },
+  mystic: {
+    name: '玄奥',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/mystic.png',
+  },
+  armor: {
+    name: '战甲',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/armor.png',
+  },
+  helmet: {
+    name: '战盔',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/helmet.png',
+  },
+  shield: {
+    name: '战盾',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/shield.png',
+  },
+  robe: {
+    name: '长袍',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/robe.png',
+  },
+  cape: {
+    name: '披风',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/cape.png',
+  },
+  mask: {
+    name: '面具',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/mask.png',
+  },
+  fur: {
+    name: '毛皮',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/fur.png',
+  },
+  scale: {
+    name: '鳞甲',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/scale.png',
+  },
+  minion: {
+    name: '爪牙',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/minion.png',
+  },
+  illusion: {
+    name: '幻象',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/illusion.png',
+  },
+  ward: {
+    name: '守卫',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/ward.png',
+  },
+  partner: {
+    name: '伙伴',
+    url: 'https://raw.githubusercontent.com/Zirpon/douyu-helper/main/src/dota2_act3/assets/img/partner.png',
+  },
 };
 
 export default act3token_url;
--- src/dota2_act3/main.js
+++ src/dota2_act3/main.js
@@ -5,35 +5,28 @@ import alertContent from './alertContent.html';
 import './assets/styles.css';
 import dota2_heros from './dota2_heros';
 
+/////////////主逻辑//////////////////////////////////////
 export default function initScript() {
   //console.log(act3_token);
   //console.log(dota2_heros);
   //console.log(act3token_url);
-  let dd = new BaseClass();
-  dd.init();
+  new BaseClass();
 }
 
-Object.keys(act3token_url).forEach(function (key) {
-  // 触发网络请求 让浏览器缓存图片
-  // https://juejin.cn/post/7340167256267391012
-  const img = new Image();
-  img.src = act3token_url[key];
-  //img.setAttribute('style', 'display:none;');
-  //document.body.appendChild(img);
-  //console.log('img', img);
-  //console.log(key, act3token_url[key]);
-});
-
+renderTokenButtonList();
 // 等待网页完成加载
 window.addEventListener(
   'load',
   function () {
+    // 因为英雄div渲染是由 JavaScript 完成 所以不能只能在load完以后再修改
     initHeroToken();
   },
   false
 );
 
-function initHeroToken() {
+/////////////接口函数/////////////////////////////////////////////
+// 展示英雄接口
+function showHeroByToken(herolist) {
   let heroElements2 = document.evaluate(
     '//*[@class="hl-wrapper"]/a[@class="HeroIcon"]',
     document,
@@ -42,8 +35,208 @@ function initHeroToken() {
     null
   );
 
+  //let grayimg = '';
   for (let i = 0; i < heroElements2.snapshotLength; i++) {
     let node = heroElements2.snapshotItem(i);
+    const href = node.getAttribute('href');
+    const name = href.split('/')[2].split('.')[0];
+
+    var svg = node.getElementsByClassName('svg-box');
+    //console.log(svg);
+    const viewBox = svg[0].getAttribute('viewBox');
+    const dddviewbox = viewBox.split(/\s+/).map(parseFloat);
+    //console.log(dddviewbox);
+
+    var ibox = node.getElementsByClassName('i-box');
+
+    //console.log(ibox[0]);
+    const height = ibox[0].clientHeight;
+    const width = ibox[0].clientWidth;
+    //console.log(height, width);
+
+    /*
+    if (grayimg == '') {
+      grayimg = this.gray(url, height, width);
+    }
+      */
+
+    if (herolist == undefined) {
+      // 展示全部英雄
+      ibox[0].style.opacity = 1; // 透明度を50%に指定
+      var tokendiv = document.getElementById('tokendiv_' + name);
+      tokendiv.style.opacity = 1; // 透明度を50%に指定
+    } else if (herolist.indexOf(name) >= 0) {
+      // 展示匹配代币的英雄
+      //console.log(name);
+      ibox[0].style.opacity = 1; // 透明度を50%に指定
+      var tokendiv = document.getElementById('tokendiv_' + name);
+      tokendiv.style.opacity = 1; // 透明度を50%に指定
+    } else {
+      // 置灰不匹配英雄
+      ibox[0].style.opacity = 0.1; // 透明度を50%に指定
+      var tokendiv = document.getElementById('tokendiv_' + name);
+      if (tokendiv) {
+        tokendiv.style.opacity = 0.1; // 透明度を50%に指定
+      } else {
+        console.log(name, ibox[0]);
+      }
+    }
+  }
+}
+
+// 代币筛选英雄接口
+function hitSearchKey(searchKeys) {
+  // searchKeys null 就是menu点击了关闭窗口 就不做任何改变
+  // 选择了代币 展示筛选英雄
+  if (searchKeys && searchKeys.length > 0) {
+    console.log(searchKeys);
+    let intersection = [];
+    let terminate = false;
+
+    Object.keys(act3_token).forEach(function (key) {
+      if (terminate) {
+        return;
+      }
+
+      if (searchKeys.indexOf(key) >= 0) {
+        //console.log(key);
+
+        let tmp = [];
+        Object.keys(act3_token[key]).forEach(function (key2) {
+          tmp = tmp.concat(act3_token[key][key2]);
+        });
+
+        //console.log(tmp);
+
+        if (intersection.length == 0) {
+          intersection = tmp;
+        } else {
+          intersection = intersection.filter((item) => tmp.includes(item));
+          //console.log(tmp, intersection);
+        }
+        if (intersection.length == 0) {
+          terminate = true;
+          return;
+        }
+      }
+    });
+    console.log(intersection);
+
+    let target_heros = Array.from(new Set(intersection));
+    if (PRODUCTION) {
+      showHeroByToken(target_heros);
+    } else {
+      let rid = new URLSearchParams(window.location.search).get('rid');
+      //console.log(rid);
+
+      if (!rid) {
+        showHeroByToken(target_heros);
+      } else {
+        let heroslist_chi = [];
+
+        Object.entries(dota2_heros).forEach(([key, value]) => {
+          if (target_heros.indexOf(key) >= 0) {
+            heroslist_chi.push(value.chi_name);
+          }
+        });
+
+        Swal2.fire(JSON.stringify(heroslist_chi));
+      }
+    }
+  } else if (searchKeys && searchKeys.length == 0) {
+    // 没选择代币 展示全部英雄
+    showHeroByToken();
+  }
+}
+
+/////////////初始化函数///////////////////////////////////////////
+// 初始化 为网页 增加 代币按钮列表
+function renderTokenButtonList() {
+  function insertBefore(node, newElement) {
+    node.insertBefore(newElement, node.firstChild);
+  }
+
+  // 代币按钮列表 样式 设置
+  const tokenlistdiv = document.createElement('div');
+  tokenlistdiv.id = 'tokenlistdiv';
+  // 横向 居中 紧凑排列
+  tokenlistdiv.style =
+    'width: 100%;height: 100%;text-align: center;justify-content:safe center;align-items: center;display: flex;flex-direction: row-reverse;';
+  // 按钮 样式 设置
+  Object.keys(act3token_url).forEach(function (key) {
+    // 触发网络请求 让浏览器缓存图片
+    // https://juejin.cn/post/7340167256267391012
+    const img = new Image();
+    img.src = act3token_url[key].url;
+    img.style = 'width:80px;height:80px;';
+    img.alt = key;
+    img.title = act3token_url[key].name;
+
+    const tokennode = document.createElement('div');
+    const tokennode_inputdiv = document.createElement('div');
+    const tokennode_input = document.createElement('input');
+    tokennode_input.type = 'checkbox';
+    tokennode_input.id = 'input_' + key;
+    tokennode_input.style.backgroundImage = "url('" + act3token_url[key].url + "')";
+    tokennode_input.style.backgroundRepeat = 'no-repeat';
+    tokennode_input.style.backgroundPosition = 'center';
+    tokennode_inputdiv.appendChild(tokennode_input);
+
+    const tokennode_text = document.createElement('div');
+    tokennode_text.innerHTML = act3token_url[key].name;
+
+    tokennode.appendChild(tokennode_inputdiv);
+    tokennode.appendChild(tokennode_text);
+
+    //console.log('img', act3token_url[key].url, act3token_url[key].name);
+
+    tokenlistdiv.appendChild(tokennode);
+    //console.log(key, act3token_url[key]);
+  });
+
+  // 添加为 hero-wrap 的第一个子节点
+  const container = document.getElementsByClassName('hero-wrap')[0];
+  insertBefore(container, tokenlistdiv);
+
+  // 为每个代币按钮 注册按键回调函数
+  var inputTag = document.getElementsByTagName('input');
+  var tokenTag = [];
+  for (var i = 0; i < inputTag.length; i++) {
+    if (inputTag[i].id.indexOf('input_') >= 0) {
+      tokenTag.push(inputTag[i]);
+    }
+  }
+  for (var i = 0; i < tokenTag.length; i++) {
+    tokenTag[i].addEventListener('change', function () {
+      var tmpoutput = [];
+
+      for (var i = 0; i < tokenTag.length; i++) {
+        if (tokenTag[i].checked) {
+          tmpoutput.push(tokenTag[i].id.split('_')[1]);
+        }
+      }
+      hitSearchKey(tmpoutput);
+    });
+
+    //console.log(tokenTag[i].id, tokenTag[i].checked);
+  }
+}
+// 给页面的每个英雄添加 3个代币属性
+function initHeroToken() {
+  let heroElements2 = document.evaluate(
+    '//*[@class="hl-wrapper"]/a[@class="HeroIcon"]',
+    document,
+    null,
+    XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+    null
+  );
+  //console.log(heroElements2);
+  //console.log(heroElements2);
+  //console.log(heroElements2.snapshotLength);
+
+  for (let i = 0; i < heroElements2.snapshotLength; i++) {
+    let node = heroElements2.snapshotItem(i);
+    //console.log(node);
     const href = node.getAttribute('href');
     const name = href.split('/')[2].split('.')[0];
 
@@ -57,7 +250,7 @@ function initHeroToken() {
     for (let index = 0; index < tokenList.length; index++) {
       const tokenstr = tokenList[index];
       const token_img = document.createElement('img');
-      token_img.src = act3token_url[tokenstr];
+      token_img.src = act3token_url[tokenstr].url;
       token_img.setAttribute('style', 'width:35px;height:35px;');
       tokendiv.appendChild(token_img);
     }
@@ -65,13 +258,10 @@ function initHeroToken() {
   }
 }
 
+// 按钮类
 class BaseClass {
   constructor() {
     this.firstRendor = true;
-    GM_registerMenuCommand('格罗德图书馆', () => this.menu());
-    GM_registerMenuCommand('reset', () => this.setHeroImg());
-  }
-  init() {
     this.alert = Swal2.mixin({
       // alert 模板 可自定义
       html: alertContent,
@@ -87,8 +277,10 @@ class BaseClass {
       showClass: { backdrop: 'swal2-noanimation' },
       hideClass: { backdrop: 'swal2-noanimation' },
     });
+    GM_registerMenuCommand('格罗德图书馆', () => this.menu());
+    GM_registerMenuCommand('reset', () => showHeroByToken());
   }
-
+  // 置灰图片函数
   gray(imgObj, height, width) {
     var canvas = document.createElement('canvas');
     var canvasContext = canvas.getContext('2d');
@@ -116,63 +308,6 @@ class BaseClass {
     return canvas.toDataURL();
   }
 
-  setHeroImg(herolist) {
-    let heroElements2 = document.evaluate(
-      '//*[@class="hl-wrapper"]/a[@class="HeroIcon"]',
-      document,
-      null,
-      XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-      null
-    );
-
-    //let grayimg = '';
-    for (let i = 0; i < heroElements2.snapshotLength; i++) {
-      let node = heroElements2.snapshotItem(i);
-      const href = node.getAttribute('href');
-      const name = href.split('/')[2].split('.')[0];
-
-      var svg = node.getElementsByClassName('svg-box');
-      //console.log(svg);
-      const viewBox = svg[0].getAttribute('viewBox');
-      const dddviewbox = viewBox.split(/\s+/).map(parseFloat);
-      //console.log(dddviewbox);
-
-      var ibox = node.getElementsByClassName('i-box');
-
-      //console.log(ibox[0]);
-      const height = ibox[0].clientHeight;
-      const width = ibox[0].clientWidth;
-      //console.log(height, width);
-
-      /*
-      if (grayimg == '') {
-        grayimg = this.gray(url, height, width);
-      }
-        */
-
-      if (herolist == undefined) {
-        ibox[0].style.opacity = 1; // 透明度を50%に指定
-        var tokendiv = document.getElementById('tokendiv_' + name);
-        tokendiv.style.opacity = 1; // 透明度を50%に指定
-      } else if (herolist.indexOf(name) >= 0) {
-        //console.log(name);
-        ibox[0].style.opacity = 1; // 透明度を50%に指定
-        var tokendiv = document.getElementById('tokendiv_' + name);
-        tokendiv.style.opacity = 1; // 透明度を50%に指定
-      } else {
-        ibox[0].style.opacity = 0.1; // 透明度を50%に指定
-        var tokendiv = document.getElementById('tokendiv_' + name);
-        if (tokendiv) {
-          tokendiv.style.opacity = 0.1; // 透明度を50%に指定
-        } else {
-          console.log(name, ibox[0]);
-        }
-      }
-    }
-  }
-  vendorHeros(herolist) {
-    this.setHeroImg(herolist);
-  }
   menu() {
     (async () => {
       const { value: searchKeys } = await this.alert.fire({
@@ -199,62 +334,7 @@ class BaseClass {
         },
       });
 
-      if (searchKeys) {
-        console.log(searchKeys);
-        let intersection = [];
-        let terminate = false;
-
-        Object.keys(act3_token).forEach(function (key) {
-          if (terminate) {
-            return;
-          }
-
-          if (searchKeys.indexOf(key) >= 0) {
-            //console.log(key);
-
-            let tmp = [];
-            Object.keys(act3_token[key]).forEach(function (key2) {
-              tmp = tmp.concat(act3_token[key][key2]);
-            });
-
-            //console.log(tmp);
-
-            if (intersection.length == 0) {
-              intersection = tmp;
-            } else {
-              intersection = intersection.filter((item) => tmp.includes(item));
-              //console.log(tmp, intersection);
-            }
-            if (intersection.length == 0) {
-              terminate = true;
-              return;
-            }
-          }
-        });
-        console.log(intersection);
-
-        let target_heros = Array.from(new Set(intersection));
-        if (PRODUCTION) {
-          this.vendorHeros(target_heros);
-        } else {
-          let rid = new URLSearchParams(window.location.search).get('rid');
-          //console.log(rid);
-
-          if (!rid) {
-            this.vendorHeros(target_heros);
-          } else {
-            let heroslist_chi = [];
-
-            Object.entries(dota2_heros).forEach(([key, value]) => {
-              if (target_heros.indexOf(key) >= 0) {
-                heroslist_chi.push(value.chi_name);
-              }
-            });
-
-            Swal2.fire(JSON.stringify(heroslist_chi));
-          }
-        }
-      }
+      hitSearchKey(searchKeys);
     })();
   }
 }
